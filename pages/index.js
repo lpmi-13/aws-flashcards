90680c18466b465cf418355732be1931fb672efb
Adam Leskis <leskis@gmail.com>
2021-02-16 07:29:03 +0000

    remove that .next jank and build an actual page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import fs from 'fs'
+import path from 'path'
 
-export default function Home() {
+const Home = ({jsonData}) => {
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <h1>
+      {jsonData.map(lambdas => (
+        <div>
+          <h2>
+            {lambdas.FunctionName}
+          </h2>
+          <p>
+            {lambdas.Description || "no description given"}
+          </p>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      ))}
+    </h1>
   )
 }
+
+export async function getStaticProps() {
+  const filename = 'lambdas.json';
+  const fileDirectory = path.join(process.cwd(), 'data')
+
+  const filePath = path.join(fileDirectory, filename)
+  const fileContents = fs.readFileSync(filePath, 'utf-8')
+
+  const lambdInfo = JSON.parse(fileContents)["Functions"]
+
+  return {
+    props: {
+      jsonData: lambdInfo,
+    }
+  }
+}
+
+export default Home;
