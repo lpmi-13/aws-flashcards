b64040b628069c29a565291224f26e5e6d835b08
Adam Leskis <leskis@gmail.com>
2021-03-11 22:54:28 +0000

    fix lambda filtering with much jq

--- lambda-script.sh
+++ lambda-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws lambda list-functions | jq '.Functions | .[] ' | jq '{name: .FunctionName, description: .Description}' > data/lambdas.json
+aws lambda list-functions | jq '{"Functions" : [.Functions[] | del(.Environment, .TracingConfig, .Role, .RevisionId, .PackageType, .CodeSha256, .FunctionArn, .LastModified)]}' > data/lambdas.json
 
