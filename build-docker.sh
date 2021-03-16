c4be965547d647bfec07129c164d009e0bb4fae2
Adam Leskis <leskis@gmail.com>
2021-03-16 00:32:27 +0000

    remove unneeded stuff

--- build-docker.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-export UID=$(id -u)
-export GID=$(id -g)
-docker build --build-arg USER=$USER \
-             --build-arg UID=$UID \
-             --build-arg GID=$GID \
-             --build-arg PW=booyah\
-             -t lambos .
